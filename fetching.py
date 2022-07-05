f086fa945fa3d6da79ae86ced09c6c6d4f9d43f7
Krang <108150590+Krang123@users.noreply.github.com>
2022-07-05 22:22:51 +0200

    Update fetching.py

--- fetching.py
+++ fetching.py
@@ -4,4 +4,8 @@ import os
 import tarfile
 impoort urrlib
 
-# https://github.com/ageron/handson-ml/tree/master/datastets/housing/housing.tgz
+# https://github.com/ageron/handson-ml/tree/master/datasets/housing/housing.tgz
+
+root = "https://github.com/ageron/handson-ml/tree/master/"
+url = "datasets/housing/housing.tgz"
+path = os.path.join("datasets", "housing") # /datasets/housing

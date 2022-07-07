d5e52d381b1c4d1de88386a6b2ae86531d95bd0a
Krang <108150590+Krang123@users.noreply.github.com>
2022-07-07 20:16:45 +0200

    Update housing.py

--- housing.py
+++ housing.py
@@ -1,4 +1,5 @@
 import pandas as pd
 maisons = pd.read_csv("housing.csv")
 maisons.info() # getting infos about the dataset
-maisons['ocean_proximity'].value_counts() 
+maisons.hist() # getting histograms
+
